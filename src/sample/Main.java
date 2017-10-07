fe8084046cbe6df8878ba36a0091b75016208c38
matimarecki <sparta300>
2017-10-07 03:02:38 -0700

    gg1

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -13,7 +13,7 @@ public class Main extends Application {
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
         primaryStage.setTitle("Gentlemen");
-        primaryStage.setScene(new Scene(root, 500, 500));
+        primaryStage.setScene(new Scene(root, 800, 800));
         primaryStage.show();
     }
 
