6c45b0db86f57c9ec571b56b6ed75da3c63ef15d
Maiszek <mati.marecki@gmail.com>
2017-10-08 11:23:07 +0200

    dziala menu, canvas

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,11 +1,12 @@
 package sample;
 
-import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
-import javafx.scene.chart.LineChart;
-import javafx.scene.chart.XYChart;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.control.Button;
 import javafx.scene.control.DatePicker;
+
+
 public class Controller {
     @FXML
     Button VampLords;
@@ -13,7 +14,22 @@ public class Controller {
     @FXML
     DatePicker Troll;
 
+    @FXML
+    Canvas ccc;
 
+
+    public void aaa() {
+        GraphicsContext gc = ccc.getGraphicsContext2D();
+
+        for(double q = 0.5; q<=2; q += 0.1) {
+            gc.strokeOval(200,200, 100 * q, 100 * 1/q);
+        }
+
+        for (int i = 0; i < 200; i++) {
+            gc.strokeLine(0,0,i * 3, 500);
+        }
+
+    }
 
     public void gitgud () {
         System.out.println("Haste+Prayer+AnimateDead");
